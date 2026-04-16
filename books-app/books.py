b3c07fd2cd48d6168e92bba4f56164061792ea53
Gaurav Gupta <gouravgupta840@gmail.com>
2026-04-16 16:46:57 +0000

    completed get with query and path var

--- books-app/books.py
+++ books-app/books.py
@@ -20,3 +20,20 @@ async def get_book(title: str):
     for book in BOOKS:
         if book.get("title").casefold() == title.casefold():
             return book
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(category: str, book_author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("author").casefold() == book_author.casefold() and book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
