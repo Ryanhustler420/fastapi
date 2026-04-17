4202f1165ea5c4f23391d68047fee1cb21449a6c
Gaurav Gupta <gouravgupta840@gmail.com>
2026-04-17 01:44:27 +0000

    added put method

--- books-app/books.py
+++ books-app/books.py
@@ -41,3 +41,9 @@ async def read_category_by_query(category: str):
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
     return { "message": "new books has been created" }
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == updated_book.get("title").casefold():
+            BOOKS[i] = updated_book
