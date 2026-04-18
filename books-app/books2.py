0baa279a2b38431f5a160c3aebfce99acc943716
Gaurav Gupta <gouravgupta840@gmail.com>
2026-04-18 14:04:07 +0000

    added validation

--- books-app/books2.py
+++ books-app/books2.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -28,11 +29,11 @@ BOOKS = [
 
 
 class BookRequest(BaseModel):
-  id: int
-  title: str
-  author: str
-  description: str
-  rating: int
+  id: Optional[int] = None
+  title: str = Field(min_length=3)
+  author: str = Field(min_length=1)
+  description: str = Field(min_length=1, max_length=100)
+  rating: int = Field(gt=-1, lt=6)
 
 
 
@@ -44,4 +45,9 @@ async def read_all_books():
 @app.post("/create-book")
 async def  create_book(book_request: BookRequest):
   new_book = Book(**book_request.dict())
-  BOOKS.append(new_book)
+  BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+  book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+  return book
