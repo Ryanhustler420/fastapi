0007a96d3fbd9187cc9b08a23c4164eac8f0004f
Gaurav Gupta <gouravgupta840@gmail.com>
2026-05-26 15:00:51 +0000

    Data Validation Query Parameters

--- books-app/books2.py
+++ books-app/books2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -62,7 +62,7 @@ async def read_book(book_id: int = Path(gt=0)):
       return book
 
 @app.get("/books/rating/")
-async def read_book_by_rating(book_rating: int):
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
   books_to_return = []
   for book in BOOKS:
     if book.rating == book_rating:
@@ -70,7 +70,7 @@ async def read_book_by_rating(book_rating: int):
   return books_to_return
 
 @app.get("/books/publish/")
-async def read_book_by_publish(published_date: int):
+async def read_book_by_publish(published_date: int = Query(gt=1900, lt=2031)):
   books_to_return = []
   for book in BOOKS:
     if book.published_date == published_date:
