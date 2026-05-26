23a29ed9622766b4ef74aa476125a08c3d1fb68a
Gaurav Gupta <gouravgupta840@gmail.com>
2026-05-26 15:24:59 +0000

    HTTP Exceptions

--- books-app/books2.py
+++ books-app/books2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -60,6 +60,7 @@ async def read_book(book_id: int = Path(gt=0)):
   for book in BOOKS:
     if book.id == book_id:
       return book
+  raise HTTPException(status_code=404, detail='Item not found')
 
 @app.get("/books/rating/")
 async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
@@ -89,13 +90,26 @@ def find_book_id(book: Book):
 
 @app.put("/books/update_book")
 async def update_book(book: BookRequest):
+  is_changes = False
+
   for i in range(len(BOOKS)):
     if BOOKS[i].id == book.id:
       BOOKS[i] = book
+      is_changes = True
+      break
+
+  if not is_changes:
+    raise HTTPException(status_code=404, detail='Item not found')
 
 @app.delete("/books/{book_id}")
 async def delete_book(book_id: int = Path(gt=0)):
+  is_changes = False
+
   for i in range(len(BOOKS)):
     if BOOKS[i].id == book_id:
       BOOKS.pop(i)
+      is_changes = True
       break
+
+  if not is_changes:
+    raise HTTPException(status_code=404, detail='Item not found')
