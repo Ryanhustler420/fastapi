a84bbafb26d4d8677e11e98db405c1e8925edbe2
Gaurav Gupta <gouravgupta840@gmail.com>
2026-06-05 15:54:13 +0000

    added first dependency

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+import typing
 import models
-from database import engine
+from models import Todos
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
 
 
